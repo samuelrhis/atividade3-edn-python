590394b16bd1a6977aec0c1dbd5fd545c1853a21
Samuel Rhis <samueloliveira327@gmail.com>
2025-06-29 21:27:19 -0300

    first commit

---
+++ circunferencia.py
@@ -0,0 +1,11 @@
+# Constante π
+PI = 3.14159265
+
+# Entrada do raio (ponto flutuante)
+raio = float(input("Digite o valor do raio: "))
+
+# Cálculo da área da circunferência
+area = PI * raio ** 2
+
+# Saída com 4 casas decimais
+print(f"A={area:.4f}")
